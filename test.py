5a493a72f9984c009f6789b99f1e438595c282ea
Victor Prates <vprates2222@gmail.com>
2025-10-27 17:20:01 -0300

    POC II

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import numpy as np
 
 from pathlib import Path
 from functools import partial
-from SemMatch.evaluation.semantics import SemanticEval
-from SemMatch.report.server import run_report
+from semmatch.evaluation.semantics import SemanticEval
+# from semmatch.report.server import run_report
 
 dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -19,34 +19,35 @@ def print_fancy(d):
     print(json.dumps(d, indent=2))
 
 if __name__ == "__main__":
-    # from reasoning.features.desc_reasoning import Reasoning, load_reasoning_from_checkpoint
+    from reasoning.features.desc_reasoning import Reasoning, load_reasoning_from_checkpoint
 
-    # def match_reasoning_model(image0, image1, model):
-    #     with torch.inference_mode():
-    #         response = model.match({
-    #             'image0': image0.unsqueeze(0).to(dev),
-    #             'image1': image1.unsqueeze(0).to(dev),
-    #         })
+    def match_reasoning_model(image0, image1, model):
+        with torch.inference_mode():
+            response = model.match({
+                'image0': image0.unsqueeze(0).to(dev),
+                'image1': image1.unsqueeze(0).to(dev),
+            })
         
-    #     mkpts0 = response['matches0'][0].detach().cpu().numpy()
-    #     mkpts1 = response['matches1'][0].detach().cpu().numpy()
+        mkpts0 = response['matches0'][0].detach().cpu().numpy()
+        mkpts1 = response['matches1'][0].detach().cpu().numpy()
     
-    #     return mkpts0, mkpts1
+        return mkpts0, mkpts1
 
-    # reasoning_model_response = load_reasoning_from_checkpoint("DescriptorReasoning_ACCV_2024/models/xfeat", "checkpoint_2_1024000.pt")
-    # model = Reasoning(reasoning_model_response['model']).to(dev).eval()
-    # match_fn = partial(match_reasoning_model, model=model)
+    reasoning_model_response = load_reasoning_from_checkpoint("DescriptorReasoning_ACCV_2024/models/xfeat", "checkpoint_2_1024000.pt")
+    model = Reasoning(reasoning_model_response['model']).to(dev).eval()
+    match_fn = partial(match_reasoning_model, model=model)
 
-    # semMatch = SemanticEval({
-    #     'sam_model': 'sam2.1_l.pt',
-    #     'data_path': 'scannet_test_1500',
-    #     'max_pairs': 15
-    # })
-
-    # semMatch.extract_and_save_matches(match_fn, 'test')
-
-    run_report({
+    semMatch = SemanticEval({
         'sam_model': 'sam2.1_l.pt',
-        'matches_file_path': Path('SemMatch/output/matches/test_matches.npz'),
-        'results_file_path': Path('SemMatch/output/reports/test_results.json')
-    }, port=6500)
+        'data_path': 'hpatches',
+        'dataset': 'hpatches',
+        'max_pairs': 15
+    })
+
+    semMatch.extract_and_save_matches(match_fn, 'test')
+
+    # run_report({
+    #     'sam_model': 'sam2.1_l.pt',
+    #     'matches_file_path': Path('SemMatch/output/matches/test_matches.npz'),
+    #     'results_file_path': Path('SemMatch/output/reports/test_results.json')
+    # }, port=6500)
